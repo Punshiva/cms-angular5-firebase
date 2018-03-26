f5266bbbe2e634e63895640aea8d342b9910807a
Shiva Pun <sp1391@live.mdx.ac.uk>
2018-03-26 18:53:47 +0100

    Add admin module with seperated routing and navbar.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCWKO1bWDOwiEgMJ3w41gNtL0tVV8SSC94',
-    authDomain: 'cms-database-cce46.firebaseapp.com',
-    databaseURL: 'https://cms-database-cce46.firebaseio.com',
-    projectId: 'cms-database-cce46',
-    storageBucket: '',
-    messagingSenderId: '103867046110'
+    apiKey: "AIzaSyDIRXlSc8m_rSV_h3MAAI8Z_IltzEzf_RU",
+    authDomain: "cmsdatabase-12cf5.firebaseapp.com",
+    databaseURL: "https://cmsdatabase-12cf5.firebaseio.com",
+    projectId: "cmsdatabase-12cf5",
+    storageBucket: "cmsdatabase-12cf5.appspot.com",
+    messagingSenderId: "446017785675"
   }
+
 };
